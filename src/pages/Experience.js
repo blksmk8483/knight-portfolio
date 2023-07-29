ead01a44ff6e14122b902737c4f9f574759a188a
Brandon Knight <knightbrandon1984@gmail.com>
2023-07-28 22:53:16 -0500

    filled in the experience page more

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -6,6 +6,7 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
 
 function Experience() {
   return (
@@ -13,14 +14,60 @@ function Experience() {
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2009-2018"
+          date="2008"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Texas A&M University-Commerce
+          </h3>
+          <p>Bachelor of Science in Music Education</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2009-2018"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
             Director of Percussion
           </h3>
           <p>Belton High School</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018-2021"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">VIPKid</h3>
+          <p>Online English Educator</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018-Present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Lavender Cove Farm
+          </h3>
+          <p>Owner and Operator</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="December 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            University of Texas at Austin
+          </h3>
+          <p>Full Stack Web Developer</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
