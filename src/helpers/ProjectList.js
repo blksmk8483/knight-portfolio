6a5ae504d8eff64b72c57e05d14f87ab841f56f9
Brandon Knight <knightbrandon1984@gmail.com>
2023-07-28 23:51:25 -0500

    added to the projects page

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,19 @@
+import Garden2 from "../assets/Garden2.gif";
+import EmployeeTracker from "../assets/Employee-tracker.png";
+import NoteTaker from "../assets/Note-Taker.png";
+
+export const ProjectList = [
+  {
+    name: "Garden2",
+    image: Garden2,
+    skills: "HTML, CSS, JavaScript, Handlebars",
+  },
+  {
+    name: "Employee Tracker",
+    image: EmployeeTracker,
+  },
+  {
+    name: "Note Taker",
+    image: NoteTaker,
+  },
+];
