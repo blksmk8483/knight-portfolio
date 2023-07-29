3ea10f54958ea020f8bfe94e93ef2f9d4720ffc2
Brandon Knight <knightbrandon1984@gmail.com>
2023-07-28 22:35:16 -0500

    added to the experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2009-2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Director of Percussion
+          </h3>
+          <p>Belton High School</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
